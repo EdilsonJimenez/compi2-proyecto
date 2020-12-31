d9da1dd6ecffe07f466cc9e6e8c00471fdcf6e43
Oscar <oscarmazarigoss5@gmail.com>
2020-12-31 12:23:24 -0600

    Correccion traduccion

--- intermedio.py
+++ intermedio.py
@@ -7,6 +7,13 @@ stack = []
 def main(): 
 	global heap
 	global stack
+
+p0="oscar"
+p1="mazariegos"
+stack.append("F2")
+
+goto .F1
+label .F2goto .END
 label .F1
 
 # Parametros 
@@ -18,16 +25,15 @@ p2
 
 # declaraciones 
 t0= 963 + 1
-t1=t0
---------- IF --------------- 
-t2= "abc" + 123
-t3= t2 > "bc"
-if t3: 
+t1 = t0
+# --------- IF --------------- 
+t2= p0 == p1
+if t2: 
 	goto .L0
 else : 
 	goto .L1
 label . L0
-     ~verdadero~
+# ~verdadero~
 goto .L2
 
 label .L1
@@ -36,18 +42,12 @@ label .L2
 goto .R
 
 
-p0=9
-p1=1
-stack.append("F2")
-
-goto .F1
-label .F2
 label .R
 u = stack[-1]
 if u == "F1": 
 	goto .F1
 if u == "F2": 
 	goto .F2
-
+label .END
 
 main() 
