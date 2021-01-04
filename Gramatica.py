85b4881d9d53d6a17552a196a7b62fc1e53abd1c
jonhiidalgo92 <53667748+jonhiidalgo92@users.noreply.github.com>
2021-01-04 16:19:32 -0600

    Intefaz
    Funciones de Retorno

--- Gramatica.py
+++ Gramatica.py
@@ -207,7 +207,7 @@ reservadas = {
 
     #Funciones
     'returns' :'RETURNS',
-    'funtion' :'FUNTION',
+    'function' :'FUNCTION',
     'language':'LANGUAGE',
     'sql'     :'SQL',
     'declare' :'DECLARE',
@@ -244,6 +244,7 @@ tokens = [
              'PARIZQ',
              'PARDER',
              'PUNTOCOMA',
+             'DOLAR',
              # NOT
              # AND
              # OR
@@ -313,6 +314,8 @@ t_LEFTSHIFT = r'<<'
 t_RIGHTSHIFT = r'>>'
 t_POTENCIA = r'\^'
 t_IGUAL2= r':='
+t_DOLAR = r'\$'
+
 
 # Importacion de Objetos Del Analisis
 
@@ -2787,7 +2790,7 @@ def p_when_auxiliar_e(t):
 
 
 def p_Funciones_General(t):
-    'FUNCIONESS  :  FUNTIONE  FUNTION  ID  PARIZQ PARAMETROSG PARDER RETURNS expresion ALIASRET CODEEPSILON DECLAEP CODE  PUNTOCOMA'
+    'FUNCIONESS  :  FUNTIONE  FUNCTION  ID  PARIZQ PARAMETROSG PARDER RETURNS expresion ALIASRET CODEEPSILON DECLAEP CODE  PUNTOCOMA'
     t[0]=Funciones_(t[1],t[3],t[8],t[9] , t[5], t[10], t[11], t[12])
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<  Estoy llegando")
 
@@ -2814,7 +2817,7 @@ def p_FuncioneeR(t):
 
 
 def p_ReservadaFunc(t):
-    'FUNCI  :  FUNTION'
+    'FUNCI  :  FUNCTION'
     t[0] = str(t[1])
 
 
@@ -2905,9 +2908,20 @@ def p_Retorno_Cuerpo(t):
 
 #------------- Alias del retorno
 def p_Alias_Retorno(t):
-    'ALIASRET  :  AS ID '
+    'ALIASRET  :  AS ALI '
     t[0] = str(t[2])
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<  ESTOY JALANDO EL ALIAS >>>>>>>>>>>>>>>>>>>>>>"+str(t[2]))
+
+
+def p_Alias_RetornoID(t):
+    'ALI  :  ID '
+    t[0] = str(t[1])
+
+def p_Alias_Retorno_DOLAR(t):
+    'ALI  :  DOLAR DOLAR '
+    t[0] = str(t[1])+str(t[2])
+
+
 
 
 def p_Alias_RetornoEpsilon(t):
@@ -2919,8 +2933,20 @@ def p_Alias_RetornoEpsilon(t):
 #-----------------------------------------------------------  SECCION DE CODIGO
 
 def p_Code_Estructures(t):
-    'CODE  : BEGIN CODEEPSILON END ARGU_N_N'
+    'CODE  : BEGIN CODEEPSILON END DOL ARGU_N_N'
     t[0] = Code_Funciones(t[4], t[2])
+
+
+
+def p_Code_Dolarcito(t):
+    'DOL  :  DOLAR DOLAR '
+    t[0] = str(t[1]) + str(t[2])
+
+
+def p_Code_DolarEpsilon(t):
+    'DOL  :  '
+    t[0] = ""
+
 
 
 #-------------------------------------  Tipos de Argumento
--- intermedio.py
+++ intermedio.py
@@ -8,65 +8,10 @@ def main():
 	global heap
 	global stack
 
-	t0 = """CREATE DATABASE IF NOT EXISTS test OWNER = root MODE = 1;"""
-	heap.append(t0)
-	F3D.ejecutarSQL()
-
-	t1 = """DROP DATABASE test;"""
-	heap.append(t1)
-	F3D.ejecutarSQL()
-
-	t2 = """USE test;"""
-	heap.append(t2)
-	F3D.ejecutarSQL()
-	#Llamada a funcion o procedimiento.
-	p0=4
-	p1=1
-	stack.append("F2")
-
-	goto .F1
-	label .F2
 
 	goto .END
 
-	label .F1
-	#**** Funcion *****
-
-	# Parametros
-	p0
-	p1
-
-	# Retorno
-	global r0
-
-	# Declaraciones
-
-	heap.append(2)
-
-	heap.append(7)
-	t3 = F3D.funcionNativa()
-	print(t3)
-
-	heap.append(2)
-	heap.append(t3)
-
-	heap.append(6)
-	t4 = F3D.funcionNativa()
-	t5 = t4
-	print(t5)
-	t6 = 25
-
-	goto .R
-
-
 	label .R
 	u = stack.pop()
-	if u == "F1":
-		goto .F1
-	if u == "F2":
-		goto .F2
 
 	label .END
-
-
-
